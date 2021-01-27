ec762a7d9772225e63dc1dc18a50ac3626bbbf3c
Arnaud Derosin <arnaud.derosin@gmail.com>
2021-01-27 20:49:54 +0900

    Create a custom UIButton, UITextField

---
+++ GithubFollowers/FavoritesListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FavoritesListViewController.swift
+//  GithubFollowers
+//
+//  Created by Arnaud DEROSIN on 2021/01/27.
+//  Copyright © 2021 Arnaud Derosin. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesListViewController: UIViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemYellow
+    }
+}
---
+++ GithubFollowers/GFButton.swift
@@ -0,0 +1,42 @@
+//
+//  GFButton.swift
+//  GithubFollowers
+//
+//  Created by Arnaud DEROSIN on 2021/01/27.
+//  Copyright © 2021 Arnaud Derosin. All rights reserved.
+//
+
+import UIKit
+
+class GFButton: UIButton
+{
+    // Used when you create the view programmatically.
+    override init(frame: CGRect)
+    {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    // Used when the view is created from storyboard/xib.
+    required init?(coder: NSCoder)
+    {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String)
+    {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    // Can only be called in this class, set up the look of the button
+    private func configure()
+    {
+        layer.cornerRadius                          = 10
+        titleLabel?.textColor                       = .white
+        titleLabel?.font                            = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints   = false
+    }
+}
