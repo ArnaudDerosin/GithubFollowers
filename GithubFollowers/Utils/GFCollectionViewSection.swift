2b82d28b63db1621c646c21955f7d05d5ef50c9e
Arnaud Derosin <arnaud.derosin@gmail.com>
2021-02-01 20:04:49 +0900

    Configure UICollectionViewDiffableDataSource.

---
+++ GithubFollowers/Utils/GFCollectionViewSection.swift
@@ -0,0 +1,14 @@
+//
+//  GFCollectionViewSection.swift
+//  GithubFollowers
+//
+//  Created by Arnaud DEROSIN on 2021/02/01.
+//  Copyright © 2021 Arnaud Derosin. All rights reserved.
+//
+
+import Foundation
+
+enum Section
+{
+    case main
+}
--- GithubFollowers/ViewControllers/FollowerListViewController.swift
+++ GithubFollowers/ViewControllers/FollowerListViewController.swift
@@ -14,7 +14,9 @@ class FollowerListViewController: UIViewController
     // MARK:- Properties
     //----------------------------------------------------------------
     var username: String!
+    var followers: [Follower] = []
     var collectionView: UICollectionView!
+    var dataSource: UICollectionViewDiffableDataSource<Section, Follower>!
     
     
     //----------------------------------------------------------------
@@ -23,24 +25,10 @@ class FollowerListViewController: UIViewController
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        configureViewcontroller()
+        configureViewController()
         configureCollectionView()
         getFollowers()
-        
-        view.backgroundColor = .systemBackground
-        navigationController?.isNavigationBarHidden = false
-        navigationController?.navigationBar.prefersLargeTitles = true
-        
-        NetworkManager.shared.getFollowers(for: username, page: 1) { result in
-            switch result
-            {
-                case.success(let followers):
-                    print(followers)
-                    
-                case.failure(let error):
-                    self.presentGFAlertOnMainThread(title: "Bad stuff Happend", message: error.rawValue, buttonTitle: "OK")
-            }
-        }
+        configureDataSource()
     }
     
     override func viewWillAppear(_ animated: Bool)
@@ -53,16 +41,18 @@ class FollowerListViewController: UIViewController
     //----------------------------------------------------------------
     // MARK:- Custom Methods
     //----------------------------------------------------------------
-    func configureViewcontroller()
+    func configureViewController()
     {
-        
+        view.backgroundColor = .systemBackground
+        navigationController?.isNavigationBarHidden = false
+        navigationController?.navigationBar.prefersLargeTitles = true
     }
     
     func configureCollectionView()
     {
         collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlowLayout())
         view.addSubview(collectionView)
-        collectionView.backgroundColor = .systemPink
+        collectionView.backgroundColor = .systemBackground
         collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
     }
     
@@ -80,8 +70,41 @@ class FollowerListViewController: UIViewController
         return flowLayout
     }
     
-    func getFollowers() {
-        
+    func getFollowers()
+    {
+        NetworkManager.shared.getFollowers(for: username, page: 1) { result in
+            switch result
+            {
+                case.success(let followers):
+                    self.followers = followers
+                    self.updateData()
+                    
+                case.failure(let error):
+                    self.presentGFAlertOnMainThread(title: "Bad stuff Happend", message: error.rawValue, buttonTitle: "OK")
+            }
+        }
     }
 
+        
+    func configureDataSource()
+    {
+        dataSource = UICollectionViewDiffableDataSource<Section, Follower>(collectionView: collectionView, cellProvider: { (collectionView, indexPath, follower) -> UICollectionViewCell? in
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FollowerCell.reuseID, for: indexPath) as! FollowerCell
+            cell.set(follower: follower)
+            
+            return cell
+        })
+    }
+    
+    func updateData()
+    {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Follower>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(followers)
+        
+        DispatchQueue.main.async
+        {
+            self.dataSource.apply(snapshot, animatingDifferences: true)
+        }
+    }
 }
