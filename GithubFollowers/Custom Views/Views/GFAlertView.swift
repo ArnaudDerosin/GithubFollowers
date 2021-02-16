dacfd353b3a34125599ae4490e646cc5ff74dea7
Arnaud Derosin <arnaud.derosin@gmail.com>
2021-02-16 12:15:14 +0900

    Clean GFAlertView / GFAlertViewController.

--- GithubFollowers/Custom Views/Views/GFAlertView.swift
+++ GithubFollowers/Custom Views/Views/GFAlertView.swift
@@ -19,7 +19,8 @@ class GFAlertView: UIView
         configure()
     }
     
-    required init?(coder: NSCoder) {
+    required init?(coder: NSCoder)
+    {
         fatalError("init(coder:) has not been implemented")
     }
     
--- GithubFollowers/ViewControllers/GFAlertViewController.swift
+++ GithubFollowers/ViewControllers/GFAlertViewController.swift
@@ -44,7 +44,7 @@ class GFAlertViewController: UIViewController
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        view.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.75)
+        view.backgroundColor = UIColor.black.withAlphaComponent(0.75)
         configureContainerView()
         configureTitleLabel()
         configureActionButton()
@@ -58,12 +58,6 @@ class GFAlertViewController: UIViewController
     private func configureContainerView()
     {
         view.addSubview(containerView)
-        
-//        containerView.backgroundColor                           = .systemBackground
-//        containerView.layer.cornerRadius                        = 16
-//        containerView.layer.borderWidth                         = 2
-//        containerView.layer.borderColor                         = UIColor.white.cgColor
-//        containerView.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
             containerView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
